bb73d20a5a18a2a4d2ad61d76eb3399ece47dcff
Mathias Holmbo <mathias.christensen@hotmail.com>
2016-12-28 22:34:08 +0100

    Removed unwanted admin menu items.

--- wp theme/themes/timewapp-theme/functions.php
+++ wp theme/themes/timewapp-theme/functions.php
@@ -9,4 +9,12 @@ function child_theme_stylesheet() {
 	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/assets/css/child.css', array(), null, 'all' );
 }
 
+/**
+ * Remove unwanted admin menu items from WordPress backend.
+ */
+add_action( 'admin_menu', 'wp_remove_menus');
+function wp_remove_menus() {
+	remove_menu_page( 'edit-comments.php' );    // Comments.
+	remove_menu_page( 'edit.php' );             // Posts (not pages).
+}
 
