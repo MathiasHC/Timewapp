6018ee9fe2c2dfbcb0d6040637128ce3e08dd5ed
Mathias Holmbo <mathias.christensen@hotmail.com>
2017-01-01 23:15:52 +0100

    Added REST API support for created Custom Post Types (those created from erp-cpt-generator).

--- wordpress/wp-content/themes/timewapp-theme/functions.php
+++ wordpress/wp-content/themes/timewapp-theme/functions.php
@@ -18,3 +18,25 @@ function wp_remove_menus() {
 	remove_menu_page( 'edit.php' );             // Posts (not pages).
 }
 
+// This is a temporary solution until register_post_type_args is available
+// After adding your CPT, you can find it at mysite.com/wp-json/wp/v2/[slug]
+function sb_add_cpts_to_api() {
+	global $wp_post_types;
+
+	// Add CPT slugs here
+	$arr = ['projects','clients','time-registrations'];
+
+	foreach( $arr as $key ) {
+
+		// If the post type doesn't exist, skip it
+		if( !$wp_post_types[$key] )
+			var_dump('<h1>' . $key . ' findes ikke</h1>');
+			continue;
+
+		$wp_post_types[$key]->show_in_rest = true;
+		$wp_post_types[$key]->rest_base = $key;
+		$wp_post_types[$key]->rest_controller_class = 'WP_REST_Posts_Controller';
+	}
+}
+add_action( 'init', 'sb_add_cpts_to_api', 30 );
+
