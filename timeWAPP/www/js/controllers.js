c50e4e6b52258d1ca7961d78529d53b43a0c6285
Mathias Holmbo <mathias.christensen@hotmail.com>
2017-01-02 11:18:59 +0100

    Added filter to controller, so that we can remove rendered HTML in the output from WP.

--- timeWAPP/www/js/controllers.js
+++ timeWAPP/www/js/controllers.js
@@ -1,5 +1,11 @@
 angular.module('app.controllers', [])
 
+    .filter('removeHTMLTags', function() {
+        return function (text) {
+            return text ? String(text).replace(/<[^>]+>/gm, '') : '';
+        }
+    })
+
     .controller('clientsCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
