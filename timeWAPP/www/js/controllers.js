1216a3138ebd84e14fba162d5069359c8e3f6f48
Mathias Holmbo <mathias.christensen@hotmail.com>
2017-01-02 02:29:50 +0100

    Added $http for constructor of controller (to use HTTP protokol for API).

--- timeWAPP/www/js/controllers.js
+++ timeWAPP/www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('app.controllers', [])
   
 .controller('clientsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+    .controller('clientsCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
