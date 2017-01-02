63f17beed54f5c7cecfd6b5205206a4f503ffe63
Mathias Holmbo <mathias.christensen@hotmail.com>
2017-01-02 14:10:15 +0100

    Wrote GET call for single project and the times connected to the project.

--- timeWAPP/www/js/controllers.js
+++ timeWAPP/www/js/controllers.js
@@ -43,18 +43,44 @@ angular.module('app.controllers', [])
                 });
         }])
 
-    .controller('singleProjectCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    .controller('singleProjectCtrl', ['$scope', '$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
         function ($scope, $http, $stateParams) {
 
-            $scope.items = [];
-            $http.get("http://dho.igniters.dk/wp-json/wp/v2/projects/" + $post_id)
-                .then(function(response) {
-                    $scope.getItems = response;
-                    return $scope.items;
+            $scope.project = [];
+            $scope.times = [];
+            $scope.project_times = [];
+            $http.get('http://dho.igniters.dk/wp-json/wp/v2/projects/' + $stateParams.projectId)
+                .then(function successCallback(response) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    // set clients[] to response.data (data is the arrays of data within the response object).
+                    $scope.project = response.data;
+                    console.log($scope.project);
+
+                    // GET all time-registrations.
+                    $http.get('http://dho.igniters.dk/wp-json/wp/v2/time-registrations')
+                        .then(function successCallback(response) {
+                            // Set $scope.times equal to response.data.
+                            $scope.times = response.data;
+                            // forEach $scope.times, do this.
+                            angular.forEach($scope.times, function (value, key) {
+                                // If ID of value equals $scope.project.id, then add time to $scope.project_times.
+                                if (value.acf.choose_project.ID == $scope.project.id) {
+                                    $scope.project_times.push(value);
+                                }
+                            })
+
+                        }, function errorCallback(response) {
+                            alert('ERROR!\n' + response)
+                        });
+
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    alert('ERROR!\n' + response);
                 });
-            console.log($scope.items);
 
         }])
 
