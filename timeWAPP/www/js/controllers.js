c216944a3c361254aa823bd01db6d98464f8ee53
Mathias Holmbo <mathias.christensen@hotmail.com>
2017-01-02 02:31:00 +0100

    Added the REST call to WP API to retrieve all projects.

--- timeWAPP/www/js/controllers.js
+++ timeWAPP/www/js/controllers.js
@@ -24,10 +24,24 @@ angular.module('app.controllers', [])
 .controller('projectsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+        function ($scope, $http, $stateParams) {
+            $scope.projects = [];
+            $http({
+                method: 'GET',
+                url:    'http://dho.igniters.dk/wp-json/wp/v2/projects'
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                // set clients[] to response.data (data is the arrays of data within the response object).
+                $scope.projects = response.data;
+                console.log($scope.projects);
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                alert('ERROR!\n' + response);
+            });
+        }])
 
-
-}])
    
 .controller('singleProjectCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
